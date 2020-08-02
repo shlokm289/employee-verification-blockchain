33e23bdc243ebe9f01020c730a10210fa2bd6c60
Shlok <shlok_mange@protonmail.com>
2020-08-02 19:54:06 +0530

    1.Setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//firebase
+import firebase from 'firebase/app';
+import {firebaseConfig} from './firebaseConfig';
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
